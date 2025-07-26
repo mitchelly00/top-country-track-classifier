f489f729825c88a3b5e90d64b93530a8db29f115
mitchelly00 <mjschonhoeft@utexas.edu>
2025-07-26 02:04:07 -0400

    test for prediction

---
+++ test/check_predictions.py
@@ -0,0 +1,19 @@
+import boto3
+import pandas as pd
+import io
+
+# S3 config
+BUCKET_NAME = "top-country-track-classifier"
+KEY = "track_metadata_with_predictions.pkl"
+
+# Create S3 client and download object
+s3 = boto3.client("s3")
+response = s3.get_object(Bucket=BUCKET_NAME, Key=KEY)
+
+# Load the pickle data into a DataFrame
+body = response['Body'].read()
+df = pd.read_pickle(io.BytesIO(body))
+
+# Print the head
+print(df.head())
+
